6b7eca9871e30d83fd9c8d7bc94be93b1570eab4
Shaurya144 <116948636+Shaurya144@users.noreply.github.com>
2023-08-01 19:30:56 +0100

    Update Python Basics.py

--- Python Basics.py
+++ Python Basics.py
@@ -14,3 +14,12 @@ print(a * b)
 print(a + b)
 print(b / a)
 print(b - a)
+
+# Coding Challenge
+import math
+
+distance_of_A_to_B = input("What is the straight line distance, in 1miles, between point A to point B.")
+in_kilometres = float(distance_of_A_to_B) * 1.6
+flightpath = math.ceil((in_kilometres * 3.142)/2)
+print("The distance between point A and point B along the flight path of the satalite is " + str(flightpath) + " kilometre.")
+
