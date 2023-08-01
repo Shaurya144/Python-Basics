71622329956902a633d822b295e14be9420b52cc
Shaurya144 <116948636+Shaurya144@users.noreply.github.com>
2023-08-01 19:15:10 +0100

    Add files via upload

---
+++ Python Basics.py
@@ -0,0 +1,16 @@
+# Variables
+day = "Monday"
+print(day)
+
+# Concatenation
+name = "Shau"
+age = "2"
+print ("Hello, " + name + " are you " + age + " years old?")
+
+# Basic Arithmetics 
+a = 12
+b = 84
+print(a * b)
+print(a + b)
+print(b / a)
+print(b - a)
